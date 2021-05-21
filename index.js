bf67d66c8e41afc88c54a0482ce3084a15f962a4
Znak Zorro <ZnakZorro@users.noreply.github.com>
2021-05-21 10:12:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(`Hello Node.js v${process.versions.node}!`);
+for (let i=0; i<3; i++) console.log(`${i} Hello Node.js v${process.versions.node}!`);
+
